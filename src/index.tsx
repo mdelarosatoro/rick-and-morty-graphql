3381af4ba5751d8a3f9486fb3d9ccba0e5d758f1
Max De La Rosa Toro <mdelarosatoroag@gmail.com>
2022-04-16 12:55:50 +0200

    Installed GraphQL and ApolloClient

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+    ApolloClient,
+    ApolloProvider,
+    HttpLink,
+    InMemoryCache,
+} from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Instantiate required constructor fields
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'https://graphqlpokemon.favware.tech/',
+});
+
+export const client = new ApolloClient({
+    cache,
+    link,
+    name: 'graphql-pokemon-client',
+    version: '1.0',
+    queryDeduplication: false,
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'cache-and-network',
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>
 );
 
